df9c533e1872eb3e86cec24e4527e7f1043aa35c
Allan Medeiros Minato <allan.minato@gmail.com>
2023-02-04 20:09:49 -0300

    Resolução exercício 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import '../style/table.css';
 
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <div>
+        Table
+        <table>
+          <tr className="table">
+            <th scope="col">Descrição</th>
+            <th scope="col">Tag</th>
+            <th scope="col">Método de pagamento</th>
+            <th scope="col">Valor</th>
+            <th scope="col">Moeda</th>
+            <th scope="col">Câmbio utilizado</th>
+            <th scope="col">Valor convertido</th>
+            <th scope="col">Moeda de conversão</th>
+            <th scope="col">Editar/Excluir</th>
+          </tr>
+        </table>
+      </div>
     );
   }
 }
